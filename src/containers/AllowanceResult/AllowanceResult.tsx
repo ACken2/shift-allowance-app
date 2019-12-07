b24822cb2d03475122ba9390a80085a30d279562
ACken2 <acken2@outlook.com>
2019-12-07 18:46:13 +0800

    Implemented AllowanceResult page

---
+++ src/containers/AllowanceResult/AllowanceResult.tsx
@@ -0,0 +1,46 @@
+// Import library
+import React from 'react';
+import { AllowanceResultUnit } from 'components/AllowanceResultUnit';
+
+// Import typings for computation result from Allowance.ts
+import { AllowanceDetail } from 'containers/App/Allowance';
+
+// Import CSS module stylesheet
+import styles from './AllowanceResult.module.css';
+
+// Setup typings for props for the AllowanceResult container
+type AllowanceResultProps = {
+	// Allowance computation result by month from Allowance.compute()
+	allowance: Array<AllowanceDetail>;
+	// Allowance computation result by month, break down by day, from Allowance.compute()
+	// It is assumed that the breakdown of allowance[i] in month view will be located at allowanceBreakdown[i],
+	// where allowanceBreakdown[i] is an array of AllowanceDetail that contained AllowanceDetail on a day-to-day basis
+	allowanceBreakdown: Array<Array<AllowanceDetail>>;
+}
+
+// Render our allowance compute result page
+const AllowanceResult: React.FC<AllowanceResultProps> = ({ allowance, allowanceBreakdown }: AllowanceResultProps) => {
+	return (
+		<div className={styles.allowanceResult}>
+			<header className={styles.allowanceResultHeader}>
+				{
+					allowance.map((a, i) => {
+						return (
+							<AllowanceResultUnit
+								key={JSON.stringify(a)}
+								allowance={a}
+								allowanceBreakdown={allowanceBreakdown[i]}
+							/>
+						)
+					})
+				}
+			</header>
+			<footer className={styles.allowanceResultFooter}>
+                <div className="center-div">
+                </div>
+            </footer>
+		</div>
+	);
+}
+
+export default AllowanceResult;
---
+++ src/containers/AllowanceResult/index.tsx
@@ -0,0 +1,2 @@
+// For export
+export { default as AllowanceResult } from './AllowanceResult';
