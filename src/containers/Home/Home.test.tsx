f4293276cd19d7ee6472ecd1650797b55757cbdc
ACken2 <acken2@outlook.com>
2020-12-04 13:30:01 +0800

    Renamed the test for <Home />

--- src/containers/Home/Home.test.tsx
+++ src/containers/Home/Home.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './Home';
 
-it('renders without crashing', () => {
+it('Renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<Home />, div);
 	ReactDOM.unmountComponentAtNode(div);
