65c00b25f3bbbf2b7cdb7e842b25541d008ecf14
Ken Sze <acken2@outlook.com>
2026-08-09 00:33:42 +0800

    Refactor ShiftDutyCalendar to use date-fns for date handling and update localizer

--- src/components/ShiftDutyCalendar/ShiftDutyCalendar.tsx
+++ src/components/ShiftDutyCalendar/ShiftDutyCalendar.tsx
@@ -1,7 +1,8 @@
 // Import library
 import React from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import { format, parse, startOfWeek, getDay } from 'date-fns';
+import { enUS } from 'date-fns/locale';
 
 // Import custom typings for libraries
 import CalendarEvent from 'containers/App/CalendarEvent';
@@ -13,35 +14,45 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './ShiftDutyCalendar.toolbar.css';
 import './ShiftDutyCalendar.month-view.css';
 
+const locales = {
+	'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+	format,
+	parse,
+	startOfWeek,
+	getDay,
+	locales,
+});
+
 // Setup typings for props for this component
 type ShiftDutyCalendarProps = {
-    style: string;
-    events: Array<CalendarEvent>;
-    onSelectSlot: Function;
-    onSelectEvent: Function;
+	style: string;
+	events: Array<CalendarEvent>;
+	onSelectSlot: Function;
+	onSelectEvent: Function;
 }
 
 // Redner our shift duty calendar
 const ShiftDutyCalendar: React.FC<ShiftDutyCalendarProps> = ({ style, events, onSelectSlot, onSelectEvent }: ShiftDutyCalendarProps) => {
-    // Set up localizer for react-big-calendar
-    const localizer = momentLocalizer(moment);
-    // Set default date (date to display by default) as the first event in events array or current date if there is no event
-    const defaultDate = events.length > 0 ? events[0].start : new Date();
-    // Render a react-big-calendar
-    return (
-        <div className={style}>
-            <Calendar
-                selectable
-                localizer={localizer}
-                events={events}
-                defaultDate={defaultDate}
-                longPressThreshold={100}
-                onSelectSlot={(selectedSlot: any) => onSelectSlot(selectedSlot)}
-                onSelectEvent={(selectedEvent: any) => onSelectEvent(selectedEvent)}
-                views={['month']}
-            />
-        </div>
-    )
+	// Set default date (date to display by default) as the first event in events array or current date if there is no event
+	const defaultDate = events.length > 0 ? events[0].start : new Date();
+	// Render a react-big-calendar
+	return (
+		<div className={style}>
+			<Calendar
+				selectable
+				localizer={localizer}
+				events={events}
+				defaultDate={defaultDate}
+				longPressThreshold={100}
+				onSelectSlot={(selectedSlot) => onSelectSlot(selectedSlot)}
+				onSelectEvent={(selectedEvent) => onSelectEvent(selectedEvent)}
+				views={['month']}
+			/>
+		</div>
+	)
 }
 
 export default ShiftDutyCalendar;
