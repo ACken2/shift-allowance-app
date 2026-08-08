a8aa81c18610a88816f189739224d33887509383
Ken Sze <acken2@outlook.com>
2026-08-09 01:06:58 +0800

    Refactor DateSelect component to use MUI DatePicker and remove theming

--- src/containers/DateSelect/DateSelect.tsx
+++ src/containers/DateSelect/DateSelect.tsx
@@ -1,10 +1,7 @@
 // Import library
 import React from 'react';
-import DateFnsUtils from '@date-io/date-fns';
-import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
-import { createTheme } from '@material-ui/core/styles';
-import { ThemeProvider } from "@material-ui/styles";
-import Button from '@material-ui/core/Button';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Button from '@mui/material/Button';
 
 // Import logo of our home page
 import logo from './calendar_logo.svg';
@@ -14,78 +11,50 @@ import styles from './DateSelect.module.css';
 
 // Setup typings for props for our DateSelect page
 type DateSelectProps = {
-    onDateConfirmed: Function;
+	onDateConfirmed: Function;
 }
 
 // Render our date select page
 const DateSelect: React.FC<DateSelectProps> = ({ onDateConfirmed }: DateSelectProps) => {
-    // <KeyboardDatePicker> requires date to be managed by class components that included it
-    // Thus, we are using React state hooks to add a selectedDate state to this function component
-    // selectedDate is the state for this component, while setSelectedDate() can be used to update the state
-    // Reference: https://reactjs.org/docs/hooks-state.html
-    const [selectedDate, setSelectedDate] = React.useState<Date | null>(
-        new Date()
-    );
-    // Use dark theme such that <KeyboardDatePicker> would be colored correctly
-    const defaultMaterialTheme = createTheme({
-        palette: {
-            type: 'dark'
-        },
-        overrides: {
-            MuiPickersDay: {
-                day: {
-                    color: '#FFFFFF'
-                },
-                current: {
-                    color: '#FF9F71'
-                }
-            },
-            MuiButton: {
-                textPrimary: {
-                    color: '#FFFFFF'
-                }
-            }
-        }
-    });
-    return (
-        <ThemeProvider theme={defaultMaterialTheme}>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <div className={styles.dateSelect}>
-                    <header className={styles.dateSelectHeader}>
-                        <img src={logo} className={styles.dateSelectLogo} alt="logo" />
-                        <div className={styles.dateSelectBody}>
-                            <p className={styles.dateSelectBodyText}>Pick the date when your shift duty begins</p>
-                            <div className={styles.datePicker}>
-                                <KeyboardDatePicker
-                                    margin="normal"
-                                    id="date-picker-dialog"
-                                    label="Beginning of your shift duty"
-                                    format="d MMMM yy, iii"
-                                    value={selectedDate}
-                                    onChange={setSelectedDate}
-                                    KeyboardButtonProps={{
-                                        'aria-label': 'change date',
-                                    }}
-                                />
-                            </div>
-                            <div className={styles.dateSelectBodyButtonDiv}>
-                                <Button variant="contained" color="primary" className={styles.dateSelectBodyButton} onClick={() => onDateConfirmed(selectedDate, 0)}>
-                                    Confirm
-                                </Button>
-                                <Button variant="contained" color="secondary" className={styles.dateSelectBodyButton} onClick={() => onDateConfirmed(selectedDate, -1)}>
-                                    Skip
-                                </Button>
-                            </div>
-                        </div>
-                    </header>
-                    <footer className={styles.dateSelectFooter}>
-                        <div className="icon-footer">
-                            Icons made by <a href="https://www.flaticon.com/authors/darius-dan" title="Darius Dan">Darius Dan</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-                        </div>
-                    </footer>
-                </div>
-            </MuiPickersUtilsProvider>
-        </ThemeProvider>
+	const [selectedDate, setSelectedDate] = React.useState<Date | null>(
+		new Date()
+	);
+	return (
+		<div className={styles.dateSelect}>
+			<header className={styles.dateSelectHeader}>
+				<img src={logo} className={styles.dateSelectLogo} alt="logo" />
+				<div className={styles.dateSelectBody}>
+					<p className={styles.dateSelectBodyText}>Pick the date when your shift duty begins</p>
+					<div className={styles.datePicker}>
+						<DatePicker
+							label="Beginning of your shift duty"
+							format="d MMMM yy, EEE"
+							value={selectedDate}
+							onChange={(date) => setSelectedDate(date)}
+							slotProps={{
+								textField: {
+									margin: 'normal',
+									id: 'date-picker-dialog',
+								},
+							}}
+						/>
+					</div>
+					<div className={styles.dateSelectBodyButtonDiv}>
+						<Button variant="contained" color="primary" className={styles.dateSelectBodyButton} onClick={() => onDateConfirmed(selectedDate, 0)}>
+							Confirm
+						</Button>
+						<Button variant="contained" color="secondary" className={styles.dateSelectBodyButton} onClick={() => onDateConfirmed(selectedDate, -1)}>
+							Skip
+						</Button>
+					</div>
+				</div>
+			</header>
+			<footer className={styles.dateSelectFooter}>
+				<div className="icon-footer">
+					Icons made by <a href="https://www.flaticon.com/authors/darius-dan" title="Darius Dan">Darius Dan</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+				</div>
+			</footer>
+		</div>
 	);
 }
 
